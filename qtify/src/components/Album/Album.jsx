7347ffa9c2f03ef5ec4c958c1418292debce9ff4
surendergupta <gupta.surender.1990@gmail.com>
2024-08-28 18:43:41 +0530

    milestone2

---
+++ qtify/src/components/Album/Album.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import styles from "./Album.module.css";
+import Chip from '@mui/material/Chip';
+import { Typography } from '@mui/material';
+const Album = ({album}) => {
+  const { title, slug, description, image: imageAlbum, id, follows, songs } = album;
+  console.log(slug);
+  console.log(songs);
+  return (
+    <>
+    {/* console.log(album) */}
+    <Card className={styles.card} key={id}>
+      <CardMedia 
+        className={styles.cardMedia}        
+        image={imageAlbum}
+        title={title}
+      />
+      <div style={{ padding: '4px 8px', borderRadius: '0px 0px 10px 10px', backgroundColor: '#fff'}} >
+        <Chip label={`${follows} Follows`}  variant="outlined" style={{ backgroundColor: 'var(--color-black)', color: '#fff'}} />
+      </div>
+      <CardContent style={{ padding: '2px'}}>
+        <Typography variant="p" component="div" style={{ color: '#fff', fontWeight: '400', fontSize: '14px', lineHeight: '21px' }}>{description}</Typography>
+      </CardContent>
+    </Card>
+    </>
+  )
+}
+
+export default Album
--- qtify/src/pages/Home.jsx
+++ qtify/src/pages/Home.jsx
@@ -1,15 +1,116 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import Navbar from '../components/Navbar/Navbar'
 import Hero from '../components/Hero/Hero'
 import styles from "./Home.module.css";
+import Album from '../components/Album/Album';
+import axios from 'axios';
 
 const Home = () => {
+    const [topAlbums, setTopAlbums] = useState([]);
+    const [topAlbumsCollapsed, setTopAlbumsCollapsed] = useState(true);
+    const [newAlbums, setNewAlbums] = useState([]);
+    const [newAlbumsCollapsed, setNewAlbumsCollapsed] = useState(true);
+
+    const handleTopAlbumsCollapseToggle = () => {
+        setTopAlbumsCollapsed(!topAlbumsCollapsed);
+    };
+
+    const handleNewAlbumsCollapseToggle = () => {
+        setNewAlbumsCollapsed(!newAlbumsCollapsed);
+    };
+
+    useEffect(() => {
+        const fetchTopAlbums = async () => {
+            try {
+                const response = await axios.get('https://qtify-backend-labs.crio.do/albums/top', {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setTopAlbums(response.data); // Use response.data to get the data from the response
+            } catch (error) {
+                console.error('Error fetching top albums:', error);
+            }
+        };
+        fetchTopAlbums();
+    }, []);
+    
+    useEffect(() => {
+        const fetchNewAlbums = async () => {
+            try {
+                const response = await axios.get('https://qtify-backend-labs.crio.do/albums/top', {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setNewAlbums(response.data); // Use response.data to get the data from the response
+            } catch (error) {
+                console.error('Error fetching top albums:', error);
+            }
+        };
+        fetchNewAlbums();
+    }, []);
+
   return (
     <>
         <Box className={styles.main}>
             <Navbar />
             <Hero />
+            <Box component="section" className={styles.sectionContainer}>
+                <div className={styles.sectionTitle}>
+                    <h3>Top Albums </h3>
+                    <div onClick={handleTopAlbumsCollapseToggle} style={{cursor: 'pointer', color: 'var(--color-primary)'}} className='pull-right'>{topAlbumsCollapsed ? 'Show all' : 'Collapse'}</div>
+                </div>
+                <div className={styles.sectionTitle}>
+                    {!topAlbumsCollapsed && (
+                        <div className={styles.albumgrid}>
+                            {topAlbums.map((album) => (
+                                <Album key={album.id} album={album} />
+                            ))}
+                        </div>
+                    )}
+                    {topAlbumsCollapsed && (
+                        <div className={styles.albumgrid}>
+                            {topAlbums.slice(0, 7).map((album) => (
+                                <Album key={album.id} album={album} />
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </Box>
+            <Box component="section" className={styles.sectionContainer}>
+                <div className={styles.sectionTitle}>
+                    <h3>New Albums </h3>
+                    <div onClick={handleNewAlbumsCollapseToggle} style={{cursor: 'pointer', color: 'var(--color-primary)'}} className='pull-right'>{newAlbumsCollapsed ? 'Show all' : 'Collapse'}</div>
+                </div>
+                <div className={styles.sectionTitle}>
+                    {!newAlbumsCollapsed && (
+                        <div className={styles.albumgrid}>
+                            {newAlbums.map((album) => (
+                                <Album key={album.id} album={album} />
+                            ))}
+                        </div>
+                    )}
+                    {newAlbumsCollapsed && (
+                        <div className={styles.albumgrid}>
+                            {newAlbums.slice(0, 7).map((album) => (
+                                <Album key={album.id} album={album} />
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </Box>
+            {/* <Box component="section" className={styles.sectionContainer}>
+                <div className={styles.sectionTitle}>
+                    <h3>Songs </h3>
+                </div>
+                <div className={styles.sectionTitle}>
+
+                </div>
+            </Box> */}
         </Box>
     </>
   )
